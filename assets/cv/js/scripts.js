cfce9c9e70544b869a7aad3e3f11b4a27a456bf3
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2018-02-05 17:57:48 +0000

    fix image link on the google map

--- assets/cv/js/scripts.js
+++ assets/cv/js/scripts.js
@@ -138,7 +138,7 @@
 
 /*----------------------------- Google Map --------------------------*/
   $('#gmap').gmap3({
-    marker:{address:"61, Bolingbroke road, CV3 1AP Coventry, United Kingdom", options:{icon: "img/location1.png"}},
+    marker:{address:"61, Bolingbroke road, CV3 1AP Coventry, United Kingdom", options:{icon: "/assets/cv/images/location1.png"}},
     map: {
         options:{
           zoom: 14,
